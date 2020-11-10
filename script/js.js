aa054cd03b9e87a055f45bfb53a6de9b15ac66a7
马威 <mawei@maweideMacBook-Pro.local>
2020-11-10 20:26:17 +0800

    mawei-1109

--- script/js.js
+++ script/js.js
@@ -45,6 +45,149 @@ $(window).scroll(function(){
 		$('.ma-system-title').removeClass('title-run')
 	}
 
+	if(scrollTop > 3650){//这里100代表你要动画的元素离最顶层的距离，console.log一下就知道了。
+		$('.ma-system-view-title').addClass('title-run')
+
+		if (scrollTop > 4700) $('.ma-system-view-title').removeClass('title-run')
+	} else {
+		
+		$('.ma-system-view-title').removeClass('title-run')
+	}
+
+	if(scrollTop > 4850){//这里100代表你要动画的元素离最顶层的距离，console.log一下就知道了。
+		$('.ma-running-title').addClass('title-run')
+
+		if (scrollTop > 5900) $('.ma-running-title').removeClass('title-run')
+	} else {
+		
+		$('.ma-running-title').removeClass('title-run')
+	}
+
+	if(scrollTop > 5300){//这里100代表你要动画的元素离最顶层的距离，console.log一下就知道了。
+		$('.ma-super-title').addClass('title-run')
+
+		if (scrollTop > 6300) $('.ma-super-title').removeClass('title-run')
+	} else {
+		
+		$('.ma-super-title').removeClass('title-run')
+	}
+
+	if(scrollTop > 6150){//这里100代表你要动画的元素离最顶层的距离，console.log一下就知道了。
+		$('.ma-lens-title').addClass('title-run')
+
+		if (scrollTop > 7120) $('.ma-lens-title').removeClass('title-run')
+	} else {
+		
+		$('.ma-lens-title').removeClass('title-run')
+	}
+
+	if(scrollTop > 7150){//这里100代表你要动画的元素离最顶层的距离，console.log一下就知道了。
+		$('.ma-time-head').addClass('title-run')
+
+		if (scrollTop > 8280) $('.ma-time-head').removeClass('title-run')
+	} else {
+		
+		$('.ma-time-head').removeClass('title-run')
+	}
+
+	if(scrollTop > 7480){//这里100代表你要动画的元素离最顶层的距离，console.log一下就知道了。
+		$('.ma-icon-title').addClass('title-run')
+
+		if (scrollTop > 8450) $('.ma-icon-title').removeClass('title-run')
+	} else {
+		
+		$('.ma-icon-title').removeClass('title-run')
+	}
+
+	if(scrollTop > 8250){//这里100代表你要动画的元素离最顶层的距离，console.log一下就知道了。
+		$('.ma-icons-title').addClass('title-run')
+
+		if (scrollTop > 9280) $('.ma-icons-title').removeClass('title-run')
+	} else {
+		
+		$('.ma-icons-title').removeClass('title-run')
+	}
+
+	if(scrollTop > 9100){//这里100代表你要动画的元素离最顶层的距离，console.log一下就知道了。
+		$('.ma-clock-title').addClass('title-run')
+
+		if (scrollTop > 10070) $('.ma-clock-title').removeClass('title-run')
+	} else {
+		
+		$('.ma-clock-title').removeClass('title-run')
+	}
+
+	if(scrollTop > 10050){//这里100代表你要动画的元素离最顶层的距离，console.log一下就知道了。
+		$('.ma-notice-title').addClass('title-run')
+
+		if (scrollTop > 11100) $('.ma-notice-title').removeClass('title-run')
+	} else {
+		
+		$('.ma-notice-title').removeClass('title-run')
+	}
+
+	if(scrollTop > 10826){//这里100代表你要动画的元素离最顶层的距离，console.log一下就知道了。
+		$('.ma-day-title').addClass('title-run')
+
+		if (scrollTop > 11868) $('.ma-day-title').removeClass('title-run')
+	} else {
+		
+		$('.ma-day-title').removeClass('title-run')
+	}
+
+	if(scrollTop > 11376){//这里100代表你要动画的元素离最顶层的距离，console.log一下就知道了。
+		$('.ma-window-head').addClass('title-run')
+
+		if (scrollTop > 12403) $('.ma-window-head').removeClass('title-run')
+	} else {
+		
+		$('.ma-window-head').removeClass('title-run')
+	}
+
+	if(scrollTop > 11962){//这里100代表你要动画的元素离最顶层的距离，console.log一下就知道了。
+		$('.ma-window-title').addClass('title-run')
+
+		if (scrollTop > 12940) $('.ma-window-title').removeClass('title-run')
+	} else {
+		
+		$('.ma-window-title').removeClass('title-run')
+	}
+	
+	if(scrollTop > 12479){//这里100代表你要动画的元素离最顶层的距离，console.log一下就知道了。
+		$('.ma-fly-title').addClass('title-run')
+
+		if (scrollTop > 13528) $('.ma-fly-title').removeClass('title-run')
+	} else {
+		
+		$('.ma-fly-title').removeClass('title-run')
+	}
+
+	if(scrollTop > 13537){//这里100代表你要动画的元素离最顶层的距离，console.log一下就知道了。
+		$('.ma-weather-title').addClass('title-run')
+
+		if (scrollTop > 14628) $('.ma-weather-title').removeClass('title-run')
+	} else {
+		
+		$('.ma-weather-title').removeClass('title-run')
+	}
+
+	if(scrollTop > 14107){//这里100代表你要动画的元素离最顶层的距离，console.log一下就知道了。
+		$('.ma-behavior-title').addClass('title-run')
+
+		if (scrollTop > 15142) $('.ma-behavior-title').removeClass('title-run')
+	} else {
+		
+		$('.ma-behavior-title').removeClass('title-run')
+	}
+
+
+
 	
 	
 });
+
+
+
+
+
+
